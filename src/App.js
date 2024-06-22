f947b7fb8534b10f554f499cc50680528adc64af
Abhishek3710 <abhiachar318@gmail.com>
2024-06-22 21:46:51 +0530

    Header and Sidebar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./components/header/Header";
+import Sidebar from "./components/sidebar/Sidebar";
+import HomeScreen from "./homeScreen/HomeScreen";
+import { Container } from "react-bootstrap";
+import "./_app.scss";
+import { useState } from "react";
+const App = () => {
+  const [sidebar, toggleSidebar] = useState(false);
 
-function App() {
+  const handleToggleSidebar = () => toggleSidebar((prevValue) => !prevValue);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header handleToggleSidebar={handleToggleSidebar} />
+      <div className="app__container border border-info">
+        <Sidebar sidebar={sidebar} handleToggleSidebar={handleToggleSidebar} />
+        <Container fluid className="app_main border-warning">
+          <HomeScreen />
+        </Container>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/video/Video.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./_video.scss";
+
+const Video = () => {
+  return (
+    <div>
+      <div>video</div>
+    </div>
+  );
+};
+
+export default Video;
