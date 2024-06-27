77702e7803a5cdb38f59fb9c3c2a5ef11ffa5f00
Abhishek3710 <abhiachar318@gmail.com>
2024-06-27 07:19:21 +0530

    Video component

--- src/components/video/Video.js
+++ src/components/video/Video.js
@@ -1,10 +1,34 @@
 import React from "react";
 import "./_video.scss";
+import { AiFillEye } from "react-icons/ai";
 
 const Video = () => {
   return (
-    <div>
-      <div>video</div>
+    <div className="video">
+      <div className="video__top">
+        <img
+          src="https://img.youtube.com/vi/13gLB6hDHR8/maxresdefault.jpg"
+          alt="NA"
+        />
+        <span>10:55</span>
+      </div>
+      <div className="video__title">
+        Advanced JavaScript Tutorial in Hindi 2020
+      </div>
+      <div className="video__details">
+        <span>
+          <AiFillEye />
+          5m Views •
+        </span>
+        <span>5 Days ago</span>
+      </div>
+      <div className="video__channel">
+        <img
+          src="https://yt3.ggpht.com/ytc/AIdro_kNaumymcxYzV7_n7_VRbQ2TRlIbU2w5csOMN0cH8jiED8=s68-c-k-c0x00ffffff-no-rj"
+          alt=""
+        />
+        <p>CodeWithHarry</p>
+      </div>
     </div>
   );
 };
